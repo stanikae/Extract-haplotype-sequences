dcdeec077c853c49cc9836959111fa55a84ea9c8
Stanford Kwenda <stankwenda@gmail.com>
2019-07-18 16:12:11 +0200

    "Add loop to write reverse sequence"

--- GetSequences.java
+++ GetSequences.java
@@ -13,9 +13,11 @@ public class GetSequences {
     public static void main(String[]args)throws Exception{
         
         ArrayList<String> searchLists= new ArrayList<String>();
-        Scanner input = new Scanner (new File("/path/to/refDPB1.fa"));
-        Scanner in = new Scanner (new File("/path/to/patient haplotype data"));
-        PrintWriter file =new PrintWriter("/path/to/output-file");
+        
+        Scanner input = new Scanner (new File("ExampleData\\refDPB1.fa")); // HLA-DPB1 nucleotide file from hg37 reference sequence
+        Scanner in = new Scanner (new File("ExampleData\\Examble01.data_rev.txt"));
+        PrintWriter fwd_seq =new PrintWriter("ExampleData\\Examble01_fwd.fa"); // fwd sequence output
+        PrintWriter rev_seq =new PrintWriter("ExampleData\\Examble01_rev.fa"); // rev sequence output
                 
         StringBuilder sb=new StringBuilder();
         
@@ -39,22 +41,42 @@ public class GetSequences {
             }
             sb.append(seq);
         }
-        
-        
+                
         int len = sb.length();
         System.out.println(len);
         
-        
+        // get sequences with haplotypes on fwd strand
         for(String j: searchLists){
                 String A[]=j.split(",");
                 int x = Integer.valueOf(A[0]);
-                int x1 = x-33042876;        // to replace hard-coded value with a variable
+                int x1 = x-33042876;
                 int x2 = A[2].length();
                 y = sb.substring(x1, x1+x2);
                 System.out.println(x+"\t"+y);
-                sb.replace(x1, x1+x2, A[5]); //replace SNPs
+                sb.replace(x1, x1+x2, A[4]); // SNPs on fwd strand
+                
         }
-        file.print(sb);
-        file.close();
+        
+        // write sequence with forward haplotypes (SNPs/indels) to file
+        fwd_seq.println(">Forward_haplotype_sequence");   
+        fwd_seq.print(sb);
+        fwd_seq.close();
+                
+        // get sequences with haplotypes on rev strand
+        for(String j: searchLists){
+                String A[]=j.split(",");
+                int x = Integer.valueOf(A[0]);
+                int x1 = x-33042876;
+                int x2 = A[2].length();
+                y = sb.substring(x1, x1+x2);
+                System.out.println(x+"\t"+y);
+                sb.replace(x1, x1+x2, A[5]); // SNPs on rev strand
+            }
+        
+        // write sequence with reverse haplotypes (SNPs/indels) to file
+        rev_seq.println(">Reverse_haplotype_sequence");
+        rev_seq.print(sb);
+        rev_seq.close();
+
     }
 }
